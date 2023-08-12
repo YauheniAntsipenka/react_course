e269df3c11dd1772413bce5129047c94e2181681
Yauheni Antsipenka <yauheni_antsipenka@epam.com>
2023-08-12 20:39:01 +0300

    add "add new course" button

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -4,6 +4,7 @@ import { CoursesProps } from './Courses.types';
 import { CourseCard } from './components/CourseCard/CourseCard';
 
 import './courses.scss';
+import { Button } from '../../common/Button/Button';
 
 export const Courses = ({ courses }: CoursesProps) => {
 	if (!courses?.length) {
@@ -15,6 +16,9 @@ export const Courses = ({ courses }: CoursesProps) => {
 	}
 	return (
 		<div className='courses'>
+			<div className='addNewCourseButton'>
+				<Button text='Add new course' onClickFunction={fakeClick} />
+			</div>
 			<ul>
 				{courses.map(({ id, title, duration, creationDate, description }) => (
 					<CourseCard
@@ -30,3 +34,7 @@ export const Courses = ({ courses }: CoursesProps) => {
 		</div>
 	);
 };
+
+function fakeClick() {
+	console.log('clicked');
+}
